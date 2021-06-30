9579d9a8c8f7576654c10601462c6631ce326820
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-06-29 21:55:46 -0700

    update scraper and resize script

--- resize_memes.sh
+++ resize_memes.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-MEMES=/home/jedbrooke/Documents/source/kmemes/memes
-OUTPUT_DIR=/home/jedbrooke/Documents/source/kmemes/memes_resized
+MEMES=/home/jedbrooke/source/kmemes/imgur_memes
+OUTPUT_DIR=/home/jedbrooke/source/kmemes/memes_resized
 for f in $(ls $MEMES); do
     if [ ! -f $OUTPUT_DIR/$f ]; then
         echo "converting $f"
         convert $MEMES/$f -resize 512x512! $OUTPUT_DIR/$f
+        if [ $? -ne 0 ]; then
+            echo "error with $f"
+            rm $MEMES/$f
+        fi
     fi
 done
