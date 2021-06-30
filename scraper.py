6cbc175badb6cfcf8c4f6bc54752d1a11678de01
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-06-29 21:55:46 -0700

    update scraper and resize script

--- scraper.py
+++ scraper.py
@@ -6,21 +6,17 @@ import time
 import sys
 
 
-DATA_PATH = "/home/jedbrooke/Documents/source/kmemes/memes.json"
-IMAGE_DIR = "/home/jedbrooke/Documents/source/kmemes/memes"
+DATA_PATH = "/home/jedbrooke/source/kmemes/memes.json"
+IMAGE_DIR = "/home/jedbrooke/source/kmemes/memes"
 DATA_URL = "http://www.reddit.com/r/memes/hot.json"
 
 def load_from_web(data_url):
     request_data = req.get(data_url)
-    if request_data.status_code >= 500:
-        print(json.loads(request_data.text)["message"],file=sys.stderr)
-        sys.exit()
-    elif request_data.status_code >= 400:
+    if request_data.status_code != 200:
         print(json.loads(request_data.text)["message"],file=sys.stderr)
         sys.exit()
 
     json_data = json.loads(request_data.text)
-    print(json_data)
     with open(DATA_PATH,"w") as fh:
         fh.write(json.dumps(json_data))
 
@@ -37,16 +33,20 @@ if __name__ == '__main__':
     
     images = os.listdir(IMAGE_DIR)
     print(images)
+    count = 0
 
     for post in json_data["data"]["children"]:
         if not post["data"]["is_video"]:
             url = post["data"]["url"]
-            if url.split("/")[-1] in images:
+            ext = url.split(".")[-1]
+            if url.split("/")[-1] in images or ext == "gifv":
                 print("skipping",url)
             else:
+                count += 1
                 print("getting",url)
-                subprocess.call(["wget","-P",IMAGE_DIR,url],stdout=sys.stdout)
-                time.sleep(1)
+                subprocess.call(["wget","-P",IMAGE_DIR,url],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                time.sleep(10)
+    print("fetched",count,"memes")
     os.remove(DATA_PATH)
 
     
