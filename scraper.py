5e7bed70b0ea84b2caa3a50b4b8392e72022f685
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-06-30 18:52:38 -0700

    begin change to api-key authed scraper

--- scraper.py
+++ scraper.py
@@ -6,9 +6,11 @@ import time
 import sys
 
 
+
 DATA_PATH = "/home/jedbrooke/source/kmemes/memes.json"
 IMAGE_DIR = "/home/jedbrooke/source/kmemes/memes"
-DATA_URL = "http://www.reddit.com/r/memes/hot.json"
+SUBREDDITS = ["memes","dankmemes","pics","itookapicture","photoshopbattles","wtfstockphotos"]
+AUTH_PATH = "auth.json"
 
 def load_from_web(data_url):
     request_data = req.get(data_url)
@@ -21,32 +23,14 @@ def load_from_web(data_url):
         fh.write(json.dumps(json_data))
 
 if __name__ == '__main__':
-    data_url = DATA_URL
-    if len(sys.argv) > 1:
-        data_url = sys.argv[1]
-
-    if not os.path.exists(DATA_PATH):
-        load_from_web(data_url)
-    
-    with open(DATA_PATH,"r") as data_file:
-        json_data = json.loads(data_file.read())
-    
-    images = os.listdir(IMAGE_DIR)
-    print(images)
-    count = 0
-
-    for post in json_data["data"]["children"]:
-        if not post["data"]["is_video"]:
-            url = post["data"]["url"]
-            ext = url.split(".")[-1]
-            if url.split("/")[-1] in images or ext == "gifv":
-                print("skipping",url)
-            else:
-                count += 1
-                print("getting",url)
-                subprocess.call(["wget","-P",IMAGE_DIR,url],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                time.sleep(10)
-    print("fetched",count,"memes")
-    os.remove(DATA_PATH)
+    with open(AUTH_PATH) as fh:
+        auth_data = json.load(fh)
+    client_auth = req.auth.HTTPBasicAuth(auth_data["client-ID"],auth_data["client-secret"])
+    post_data = {"grant_type": "password", "username": auth_data["username"], "password": auth_data["password"]}
+    headers = {"User-Agest": "Kmemes_bot/0.1 by jedbrooke"}
+    auth_response = req.post("https://www.reddit.com/api/v1/acces_token", auth=client_auth, data=post_data, headers=headers)
+    print(auth_response.status_code)
+    print(auth_response.headers)
+    print(auth_response.text)
 
     
